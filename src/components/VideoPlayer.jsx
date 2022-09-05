7c1673399cc3e4c5ea3b005ff3d6e6353b9fb1c8
MohammadJavad14 <mrph14@gamil.com>
2022-09-05 22:06:37 +0430

    scroll issue and syncing subtitle fixed

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -10,14 +10,14 @@ const subtitles = subtitleFile;
 const subtitlesFa = subtitleFaFile;
 
 function VideoPlayer({ src }) {
+  const [videoContainerElement, setVideoContainerElement] = useState(null);
   const [videoElement, setVideoElement] = useState(null);
   const [trackElement, setTrackElement] = useState(null);
   const [subtitleId, setSubtitleId] = useState(null);
   const [selectedSentence, setSelectedSentence] = useState([]);
   const [videoWorks, setVideoWorks] = useState(null);
 
-  console.log("renderrrr");
-
+  const videoContainerRef = useRef(null);
   const videoRef = useRef(null);
   const trackRef = useRef(null);
   const subtitlesRef = useRef(null);
@@ -29,21 +29,26 @@ function VideoPlayer({ src }) {
 
   const scrollSmoothHandler = (index) => {
     const element = scrollRefs.current[index].current;
-    const yOffset = -410;
+    const yOffset = -videoContainerElement.offsetHeight;
     const y =
       element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-
     window.scrollTo({ top: y, behavior: "smooth" });
+    // scrollRefs.current[index].current.scrollIntoView({ behavior: "smooth" });
   };
 
   useEffect(() => {
+    setVideoContainerElement(videoContainerRef.current);
     setVideoElement(videoRef.current);
     setTrackElement(trackRef.current);
     setVideoWorks(!!document.createElement("video").canPlayType);
   }, []);
 
+  useEffect(() => {
+    setVideoContainerElement(videoContainerRef.current);
+  }, [subtitleId]);
+
   if (videoWorks) {
-    videoElement.controls = true;
+    videoContainerElement.controls = true;
   }
 
   if (trackElement)
@@ -52,7 +57,8 @@ function VideoPlayer({ src }) {
       if (cues.length === 1) {
         const id = parseInt(cues["0"].id);
         setSubtitleId(id);
-        if (id > 1) scrollSmoothHandler(id - 2);
+        console.log(videoElement.currentTime);
+        if (id > 2) scrollSmoothHandler(id - 2);
       }
     });
 
@@ -70,64 +76,67 @@ function VideoPlayer({ src }) {
     togglePaly();
   };
 
+  const handleListSubtitleClicked = (index) => {
+    videoElement.currentTime = subtitles[index].startTime;
+  };
+
   return (
     <>
-      <div>
-        <div>
-          <video
-            ref={videoRef}
-            id="video"
-            controls
-            preload="metadata"
-            poster="poster.jpg"
-            width="100%"
-            height="100%"
-          >
-            <source src={src} type="video/mp4" />
+      <div ref={videoContainerRef} className="sticky top-0">
+        <video
+          ref={videoRef}
+          id="video"
+          controls
+          preload="metadata"
+          poster="poster.jpg"
+          width="100%"
+          height="100%"
+          className="md:h-[50vh]"
+        >
+          <source src={src} type="video/mp4" />
 
-            <track
-              ref={trackRef}
-              src="demo2-en.vtt"
-              kind="metadata"
-              srcLang="en"
-              default
-            />
-          </video>
-        </div>
-        <div className="bg-bg-dark1">
+          <track
+            ref={trackRef}
+            src="demo2-en.vtt"
+            kind="metadata"
+            srcLang="en"
+            default
+          />
+        </video>
+        <div className="bg-bg-dark1 min-h-[8vh] py-1">
           {subtitleId && (
-            <div>
-              <div>
+            <>
+              <div className="flex flex-row flex-wrap justify-center py-1 px-2">
                 {subtitles[subtitleId - 1]["word_tag"].map((word, index) => (
-                  <div>
-                    <p
-                      key={index}
-                      onClick={() => handleSubtitleClick(word[0], subtitleId)}
-                    >
-                      {word[0]}
-                    </p>
-                  </div>
+                  <p
+                    className="text-[#fff] font-bold px-0.5 cursor-pointer"
+                    key={index}
+                    onClick={() => handleSubtitleClick(word[0], subtitleId)}
+                  >
+                    {word[0]}
+                  </p>
                 ))}
               </div>
-              <div>
-                <p className="text-text-gold">
-                  {subtitlesFa[subtitleId - 1].text}
-                </p>
-              </div>
-            </div>
+              <p className="text-text-gold font-bold text-center py-1">
+                {subtitlesFa[subtitleId - 1].text}
+              </p>
+            </>
           )}
         </div>
       </div>
-      <div className="bg-bg-dark2">
-        <div ref={subtitlesRef}>
-          {subtitles.map((subtitle, index) => (
-            <div key={index} ref={scrollRefs.current[index]}>
-              <p className="text-text-gray" key={index}>
-                {subtitle.text}
-              </p>
-            </div>
-          ))}
-        </div>
+      <div ref={subtitlesRef} className="bg-bg-dark2 ">
+        {subtitles.map((subtitle, index) => (
+          <p
+            ref={scrollRefs.current[index]}
+            key={index}
+            className={`${
+              subtitleId - 1 === index ? "text-white" : "text-text-gray"
+            } text-center py-3 cursor-pointer`}
+            onClick={() => handleListSubtitleClicked(index)}
+          >
+            {subtitle.text}
+          </p>
+        ))}
       </div>
     </>
   );
